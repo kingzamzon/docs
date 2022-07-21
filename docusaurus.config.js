c1dfbcbcb2d7e0d63dc769024b1dd37310105bd5
debie <debbs.ly@gmail.com>
2022-07-21 11:11:23 +0200

    Remove the homepage and link directly into docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl: "https://github.com/LIT-Protocol/docs/edit/main/website/",
+          routeBasePath: "/",
         },
         // blog: {
         //   showReadingTime: true,
