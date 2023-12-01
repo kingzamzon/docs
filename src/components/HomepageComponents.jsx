3c3bb26cf18a703c580c9d47a7ea3b731f48ddf5
sarahzdev <sarah.hg.zhang@gmail.com>
2023-12-01 10:22:56 -0800

    feat: styling

--- src/components/HomepageComponents.jsx
+++ src/components/HomepageComponents.jsx
@@ -29,12 +29,20 @@ export function HomepageSection({
 
 export function HomepageCard({ id, icon, title, description, to }) {
   return (
-    <Link to={to} className="homepage-card">
-      {icon && <div className="icon">{icon}</div>}
-      <div className="homepage-card__content">
+    // <Link to={to} className="homepage-card">
+    //   {icon && <div className="icon">{icon}</div>}
+    //   <div className="homepage-card__content">
+    //     <h6 className="homepage-card__title">{title}</h6>
+    //     <span className="homepage-card__description">{description}</span>
+    //   </div>
+    // </Link>
+    <div className="homepage-card">
+      <a href={to} className='homepage-card__link'></a>
+       <div className="homepage-card__content">
+        {icon && <span className="homepage-card__icon">{icon}</span>}
         <h6 className="homepage-card__title">{title}</h6>
         <span className="homepage-card__description">{description}</span>
       </div>
-    </Link>
+    </div>
   );
 }
