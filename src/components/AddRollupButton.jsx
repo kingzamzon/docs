97289e93d3a8cad1b2852dc26cb80b3b212b2db9
Wyatt Barnes <me@wyatt.email>
2024-07-11 11:11:27 -1000

    Update AddRollupButtons

--- src/components/AddRollupButton.jsx
+++ src/components/AddRollupButton.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
 export default function AddRollupButton() {
   const addNetwork = () => {
     const params = [
       {
-        chainId: "0x2AC49",
-        chainName: "Chronicle - Lit Protocol Testnet",
+        chainId: '0x2AC49',
+        chainName: 'Chronicle - Lit Protocol Testnet',
         nativeCurrency: {
-          name: "LIT",
-          symbol: "LIT",
+          name: 'Lit Protocol - Chronicle Testnet Token (tstLIT)',
+          symbol: 'tstLIT',
           decimals: 18,
         },
-        rpcUrls: ["https://chain-rpc.litprotocol.com/replica-http"],
-        blockExplorerUrls: ["https://chain.litprotocol.com"],
+        rpcUrls: ['https://chain-rpc.litprotocol.com/replica-http'],
+        blockExplorerUrls: ['https://chain.litprotocol.com'],
       },
     ];
 
     window.ethereum
-      .request({ method: "wallet_addEthereumChain", params })
-      .then(() => console.log("Success"))
-      .catch((error) => console.log("Error", error.message));
+      .request({ method: 'wallet_addEthereumChain', params })
+      .then(() => console.log('Success'))
+      .catch(error => console.log('Error', error.message));
   };
 
   return <button onClick={addNetwork}>Add Chronicle to Metamask</button>;
